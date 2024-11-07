0281fbdf9cc20e044fcda57904a4efdb9e448546
Kim Min Gyu <ysjk2003@gmail.com>
2024-11-07 21:21:49 +0900

    add: logo, favicon

--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../logo.svg';
+import logo from '../../asset/Logo.png';
 import './Home.css';
 import { useNavigate } from 'react-router-dom';
 import Button from '../../component/button/Button';
